623273faa32513a3a2fd7e216b211a73e09bb5c7
Juan Zuluaga <jzuluaga55@gmail.com>
2019-11-20 18:47:07 -0500

    Factorial Android

---
+++ Factorial.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Factorial.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Factorial.Core/Services/FactorialService.cs
@@ -0,0 +1,15 @@
+﻿namespace Factorial.Core.Services
+{
+    public class FactorialService : IFactorialService
+    {
+        public double CalculateFactorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
