9970556db49529aae383696603dd1a29c667405c
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2019-11-20 11:30:38 -0500

    Finish iOS

---
+++ TipCalculator.iOS/AppDelegate.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using TipCalculator.Core;
+
+namespace TipCalculator.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+    }
+}
+
+
---
+++ TipCalculator.iOS/Views/HomeView.cs
@@ -0,0 +1,26 @@
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Presenters.Attributes;
+using MvvmCross.Platforms.Ios.Views;
+using TipCalculator.Core.ViewModels;
+
+namespace TipCalculator.iOS.Views
+{
+    [MvxRootPresentation(WrapInNavigationController = true)]
+    public partial class HomeView : MvxViewController
+    {
+        public HomeView() : base("HomeView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<HomeView, TipViewModel>();
+            set.Bind(AmountText).To(vm => vm.SubTotal);
+            set.Bind(GenerositySlider).To(vm => vm.Generosity);
+            set.Bind(TipLabel).To(vm => vm.Tip);
+            set.Apply();
+        }
+    }
+}
