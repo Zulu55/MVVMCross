8e519ed3a27289a23b5aadd74c9fa6db2a977488
Juan Zuluaga <jzuluaga55@gmail.com>
2019-11-20 19:44:07 -0500

    Test

---
+++ Factorial.iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Factorial.Core;
+using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+
+namespace Factorial.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<App>, App>
+    {
+    }
+}
